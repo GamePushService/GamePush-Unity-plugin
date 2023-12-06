9dd8906c47ff59796a134ea95462b37365f64745
Aristarkh <45267374+Kerioth@users.noreply.github.com>
2023-12-06 14:38:27 +0300

    Add Device scene

--- Demo/Assets/Examples/Modules/Device/Device.cs
+++ Demo/Assets/Examples/Modules/Device/Device.cs
@@ -1,13 +1,46 @@
 using UnityEngine;
-
+using UnityEngine.UI;
+using Examples.Console;
 using GamePush;
 
 namespace Examples.Device
 {
     public class Device : MonoBehaviour
     {
-        public void IsMoble() => GP_Device.IsMobile();
+        [SerializeField] private Button _isMobileButton;
+        [SerializeField] private Button _isPortraitButton;
 
-        public void IsDesktop() => GP_Device.IsDesktop();
+        private void OnEnable()
+        {
+            _isMobileButton.onClick.AddListener(IsMobile);
+            _isPortraitButton.onClick.AddListener(IsPortrait);
+
+            GP_Device.OnChangeOrientation += OrientationChanged;
+        }
+        private void OnDisable()
+        {
+            _isMobileButton.onClick.RemoveListener(IsMobile);
+            _isPortraitButton.onClick.RemoveListener(IsPortrait);
+
+            GP_Device.OnChangeOrientation -= OrientationChanged;
+        }
+
+
+        void OrientationChanged()
+        {
+            ConsoleUI.Instance.Log("Device orientation changed");
+        }
+
+        public void IsMobile()
+        {
+            bool isMobile = GP_Device.IsMobile();
+            ConsoleUI.Instance.Log("Is Mobile: " + isMobile);
+        }
+
+        public void IsPortrait()
+        {
+           bool isPortrait = GP_Device.IsPortrait();
+           ConsoleUI.Instance.Log("Is Portrait: " + isPortrait);
+        }
     }
 }
