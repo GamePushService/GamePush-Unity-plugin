5f300f9b4dd2839ee464dcd16d554e15600c26d1
Aristarkh <45267374+Kerioth@users.noreply.github.com>
2024-05-29 18:52:07 +0300

    Added some files from native plugin

--- Demo/Assets/Plugins/GamePush/Runtime/Utilities/GP_Utility.cs
+++ Demo/Assets/Plugins/GamePush/Runtime/Utilities/GP_Utility.cs
@@ -155,17 +155,6 @@ namespace GamePush.Utilities
 
 namespace GamePush
 {
-    public class GP_Data
-    {
-        private string _data;
-        public string Data => _data;
-
-        public GP_Data(string data) => _data = data;
-
-        public T Get<T>() => UtilityJSON.Get<T>(_data);
-        public List<T> GetList<T>() => UtilityJSON.GetList<T>(_data);
-        public T[] GetArray<T>() => UtilityJSON.GetArray<T>(_data);
-    }
 
     public class Console
     {
