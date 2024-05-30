2cd8c03412594db6391ed8416de02834a60015c7
Aristarkh <45267374+Kerioth@users.noreply.github.com>
2024-05-30 09:32:07 +0300

    ProjectData set file

---
+++ Demo/Assets/Plugins/GamePush/Data/SavedProjectData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GamePush.Data
+{
+    [System.Serializable]
+    public class SavedProjectData
+    {
+        public string id, token;
+
+        public SavedProjectData(string id, string token)
+        {
+            this.id = id;
+            this.token = token;
+        }
+    }
+
+}
