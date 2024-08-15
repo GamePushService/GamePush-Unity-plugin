508f03b73c9a82c55f2703db2b6f9fb7d3c5732a
Aristarkh <45267374+Kerioth@users.noreply.github.com>
2024-08-15 16:26:10 +0300

    Update GP_Platform.cs

--- Demo/Assets/Plugins/GamePush/Runtime/Modules/GP_Platform.cs
+++ Demo/Assets/Plugins/GamePush/Runtime/Modules/GP_Platform.cs
@@ -42,11 +42,12 @@ namespace GamePush
         private static extern string GP_Platform_Tag();
         public static string Tag()
         {
+            if(Type() != Platform.CUSTOM)
+                return "";
 #if !UNITY_EDITOR && UNITY_WEBGL
             return GP_Platform_Tag();
 #else
 
-            ConsoleLog("TAG: Editor");
             return "Editor";
 #endif
         }
