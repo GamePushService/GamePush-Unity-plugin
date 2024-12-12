61e130f1d122fd862533df40b5c6998dbbce7b9f
Aristarkh <45267374+Kerioth@users.noreply.github.com>
2024-12-13 02:46:54 +0300

    Modules data remove spaces

--- Demo/Assets/Plugins/GamePush/Data/ModulesData/PlayerModuleData.cs
+++ Demo/Assets/Plugins/GamePush/Data/ModulesData/PlayerModuleData.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Collections.Generic;
-using Newtonsoft.Json;
 
 namespace GamePush.Data
 {
-
     [Serializable]
     public enum SyncStorageType
     {
---
+++ Demo/Assets/Plugins/GamePush/Data/ModulesData/SocialsModuleData.cs
@@ -0,0 +1,23 @@
+﻿namespace GamePush.Data
+{
+    [System.Serializable]
+    public enum ShareType
+    {
+        share,
+        post,
+        invite
+    }
+
+    [System.Serializable]
+    public enum SocialType
+    {
+        vkontakte,
+        odnoklassniki,
+        telegram,
+        twitter,
+        facebook,
+        moymir,
+        whatsapp,
+        viber
+    }
+}
