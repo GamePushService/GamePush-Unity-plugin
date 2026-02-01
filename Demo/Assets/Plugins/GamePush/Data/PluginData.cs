46f1981307d95dc4579675c1ce052c561471d965
Aristarkh <45267374+Kerioth@users.noreply.github.com>
2026-02-01 18:28:06 +0300

    Update PluginData.cs

--- Demo/Assets/Plugins/GamePush/Data/PluginData.cs
+++ Demo/Assets/Plugins/GamePush/Data/PluginData.cs
@@ -2,7 +2,7 @@ namespace GamePush.Data
 {
     public static class PluginData
     {
-        public const string SDK_VERSION = "1.8.1";
+        public const string SDK_VERSION = "1.8.2";
         public const string GAME_ENGINE = "Unity";
         public static bool COMPRESS_BUILD = true;
     }
